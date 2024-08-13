df3478e829339f8d30202aba6aca3d630f6f4f32
rajsome <132190989+rajsome@users.noreply.github.com>
2024-08-13 15:27:12 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("POMI5","POMI5")
+        ERC721("POMI6","POMI6")
         Ownable(initialOwner)
     {}
 
